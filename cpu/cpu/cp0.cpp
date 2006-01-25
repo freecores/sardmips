d146831f4981add43ffce16a191f10fc6ef16cf8
igorloi <igorloi@38247058-90b6-4812-a95d-7290e3d26b75>
2006-01-25 16:47:12 +0000

    *** empty log message ***

--- cpu/cpu/cp0.cpp
+++
@@ -1,4 +0,0 @@
-//
-// $Id: cp0.cpp,v 1.1 2006-01-21 03:37:32 igorloi Exp $
-//
-#include "cp0.h"
--- cpu/cpu/mem_stage.cpp
+++
@@ -1,5 +0,0 @@
-//! MEM Stage module
-//
-// $Id: mem_stage.cpp,v 1.1 2006-01-21 03:37:32 igorloi Exp $
-//
-#include "mem_stage.h"
